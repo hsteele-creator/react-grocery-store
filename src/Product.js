62ed02d61bac1fda0378fa4fb64199b1a232e2a8
Harry Steele <harrybsteele@gmail.com>
2023-06-23 16:51:50 -0400

    set up categories

--- src/Product.js
+++ src/Product.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./Product.css";
 
@@ -6,13 +6,26 @@ const Product = ({ image, name, price }) => {
   const dispatch = useDispatch();
   const store = useSelector((state) => state);
   console.log(store);
+  const [quantity, setQuantity] = useState(1);
   return (
     <>
       <div className="product-card">
         <img className="product-image" src={image} />
         <h1>{name}</h1>
-        <p> Now <span>Just</span> ${price}</p>
-        <button className="product-btn">Buy Now</button>
+        <p>
+          {" "}
+          Now <span>Just</span> ${price}
+        </p>
+
+        <div id="purchase-container">
+          <input
+            id="qty"
+            className="qty"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          ></input>
+          <button className="product-btn">Add To Cart</button>
+        </div>
       </div>
     </>
   );
--- src/Products.js
+++ src/Products.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Product from "./Product";
-import "./Products.css"
+import "./Products.css";
 
 const Products = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products);
+
+  const [type, setType] = useState("meat");
+
+  const updateProducts = () => {
+    if (type === "") {
+      return products;
+    } else {
+      return products.filter((product) => product.category === type);
+    }
+  };
+
+  const updatedProducts = updateProducts();
+
   return (
     <>
+    <div id="mini-nav">
+        <p onClick={() => setType("")}>All Products</p>
+        <p onClick={() => setType("meat")}>Meat</p>
+        <p onClick={() => setType("fruit")}>Fruit</p>
+        <p onClick={() => setType("drink")}>Drinks</p>
+    </div>
       <div className="products-container">
-        {products.map((product) => {
+        {updatedProducts.map((product) => {
           return (
             <Product
               image={product.image}
